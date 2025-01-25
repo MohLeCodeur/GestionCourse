30f96838acfd2e7a654090480444de0cf183db90
mohlecodeur <mohlecodeur@gmail.com>
2025-01-25 10:26:15 +0000

    delete success

--- Gestionlistecourse/courses/urls.py
+++ Gestionlistecourse/courses/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.liste_articles, name='liste_articles'),
     path('ajouter/', views.ajouter_article, name='ajouter_article'),
+     path('supprimer/<int:article_id>/', views.supprimer_article, name='supprimer_article'),
 ]
